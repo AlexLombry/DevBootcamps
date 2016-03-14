580c902e8b65e9eca9e35bd01156993ef1664ff0
Alex Lombry <alex.lombry@gmail.com>
2016-03-14 11:43:45 +0100

    Add delegate, personnal location

--- DevBootcamps/LocationVC.swift
+++ DevBootcamps/LocationVC.swift
@@ -9,19 +9,44 @@
 import UIKit
 import MapKit
 
-class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var map: MKMapView!
     
+    // permission available for user (user can denied it)
+    let locationmanager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // mandatory
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    
+    func locationAuthStatus() {
+        // have the user authorise app location when is currently use.
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            // We can show the current user location
+            map.showsUserLocation = true
+        } else {
+            // ask user if we can use location
+            locationmanager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    /// MARK
+    /// Initiate the Table View (mandatory func)
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
@@ -37,6 +62,5 @@ class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
 
     }
-
 }
 
